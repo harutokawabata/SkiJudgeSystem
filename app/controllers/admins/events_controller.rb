d1e2d64bd05f21c3772c0d43d584813894ad913b
EC2 Default User <ec2-user@ip-172-31-46-116.ap-northeast-1.compute.internal>
2021-03-11 15:02:52 +0000

    event-function-create

--- app/controllers/admins/events_controller.rb
+++ app/controllers/admins/events_controller.rb
@@ -1,16 +1,34 @@
 class Admins::EventsController < ApplicationController
   def new
+    @events = Event.all
   end
 
   def edit
   end
+  
+  def index
+    @event = Event.new
+    @events = Event.all 
+  end
 
   def create
+    @event = Event.new(event_params)
+    if @event.save
+      redirect_to root_path
+    else
+      flash[:notice] = '投稿に失敗しました'
+    end
+    
   end
 
   def update
   end
 
   def destroy
+  end
+  
+  private
+  def event_params
+    params.require(:event).permit(:title, :event1, :event2, :event3, :event4, :event5, :capacity, :start, :finish, :advice)
   end
 end
